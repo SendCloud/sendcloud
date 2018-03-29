d988bac5bc6f831c19ac72598ca162e6830cb03d
rick <r.vanoosten@creativeict.nl>
2018-03-29 17:03:45 +0200

    Added extension_attributes to api V1/orders

---
+++ Observer/Sales/OrderLoadAfter.php
@@ -0,0 +1,36 @@
+<?php
+namespace CreativeICT\SendCloud\Observer\Sales;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+class OrderLoadAfter implements ObserverInterface
+{
+    public function execute(Observer $observer)
+    {
+        $order = $observer->getOrder();
+        $extensionAttributes = $order->getExtensionAttributes();
+
+        if ($extensionAttributes === null) {
+            $extensionAttributes = $this->getOrderExtensionDependency();
+        }
+
+        $extensionAttributes->setSendcloudServicePointId($order->getData('sendcloud_service_point_id'));
+        $extensionAttributes->setSendcloudServicePointName($order->getData('sendcloud_service_point_name'));
+        $extensionAttributes->setSendcloudServicePointStreet($order->getData('sendcloud_service_point_street'));
+        $extensionAttributes->setSendcloudServicePointHouseNumber($order->getData('sendcloud_service_point_house_number'));
+        $extensionAttributes->setSendcloudServicePointZipCode($order->getData('sendcloud_service_point_zip_code'));
+        $extensionAttributes->setSendcloudServicePointCity($order->getData('sendcloud_service_point_city'));
+
+        $order->setExtensionAttributes($extensionAttributes);
+    }
+
+    private function getOrderExtensionDependency()
+    {
+        $orderExtension = \Magento\Framework\App\ObjectManager::getInstance()->get(
+            '\Magento\Sales\Api\Data\OrderExtension'
+        );
+
+        return $orderExtension;
+    }
+}
