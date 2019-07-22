73815beea3c97ec8b5ba5ccf8fc2d481dd41f99e
Tjitse Efdé <tjitse@vendic.nl>
2019-07-22 10:34:25 +0200

    Fix for #24  not found

--- Block/Checkout/Config.php
+++ Block/Checkout/Config.php
@@ -1,10 +1,4 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rick
- * Date: 5-4-18
- * Time: 15:44
- */
+<?php declare(strict_types=1);
 
 namespace SendCloud\SendCloud\Block\Checkout;
 
@@ -13,16 +7,8 @@ use Magento\Store\Model\ScopeInterface;
 
 class Config extends Template
 {
-    public function __construct(Template\Context $context, array $data = [])
-    {
-        parent::__construct($context, $data);
-    }
-
-    /**
-     * @return mixed
-     */
     public function getScriptUrl()
     {
-        return $this->scopeConfig->getValue('sendcloud/sendcloud/script_url', ScopeInterface::SCOPE_STORE);
+        return $this->_scopeConfig->getValue('sendcloud/sendcloud/script_url', ScopeInterface::SCOPE_STORE);
     }
 }
