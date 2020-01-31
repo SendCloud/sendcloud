4b182105a71fd066cd5fc3646b69e4e9300cda11
Aderik Teekman <a.teekman@creativeict.nl>
2020-01-31 15:21:01 +0100

    Remove some javascript errors

--- view/frontend/web/js/mixins/checkout/view/shipping-mixin.js
+++ view/frontend/web/js/mixins/checkout/view/shipping-mixin.js
@@ -6,8 +6,13 @@ define([
     return function (Component) {
         return Component.extend({
             validateShippingInformation: function() {
-                var origResult = this._super(),
-                    servicePointData = JSON.parse(window.sessionStorage.getItem('service-point-data'));
+                try{
+                    var origResult = this._super(),
+                        servicePointData = JSON.parse(window.sessionStorage.getItem('service-point-data'));
+                }
+                catch(error) {
+                    return false;
+                }
 
                 if (quote.shippingMethod()['carrier_code'] === 'sendcloud' && !servicePointData) {
                     this.errorValidationMessage($t('Please select a service point'));
