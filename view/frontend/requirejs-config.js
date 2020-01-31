eefeabd3e2c676e7e69982ce471356a144640e98
Aderik Teekman <a.teekman@creativeict.nl>
2020-01-31 01:43:58 +0100

    Fix mixin

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -6,11 +6,14 @@ var config = {
             'Magento_Checkout/js/view/payment/default': 'SendCloud_SendCloud/js/view/payment/default'
         }
     },
-    "config": {
+    config: {
         mixins: {
             'Magento_Checkout/js/view/shipping-information': {
                 'SendCloud_SendCloud/js/mixins/checkout/view/shipping-information': true
             },
+            'Magento_Checkout/js/view/shipping': {
+                'SendCloud_SendCloud/js/mixins/checkout/view/shipping-mixin': true
+            }
         }
     }
 };
