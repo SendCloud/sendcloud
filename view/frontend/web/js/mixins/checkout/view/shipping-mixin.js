2d21261e257f613d60040c7ee14a994f6fb0fe70
Rick van Oosten <r.vanoosten@creativeict.nl>
2020-02-06 11:56:56 +0100

    Bugfix/amasty magento234 (#54)
    * Update composer.json
    
    * Feature/amasty (#52)
    
    * Fix mixin
    * Remove some javascript errors
    
    * bugfix/amasty-magento234 | fixed translation error
    
    Co-authored-by: Aderik Teekman <aderik@live.nl>

---
+++ view/frontend/web/js/mixins/checkout/view/shipping-mixin.js
@@ -0,0 +1,27 @@
+define([
+    'Magento_Checkout/js/model/quote',
+    'mage/translate'
+], function (quote, $t) {
+    'use strict';
+
+    return function (Component) {
+        return Component.extend({
+            validateShippingInformation: function() {
+                try{
+                    var origResult = this._super(),
+                        servicePointData = JSON.parse(window.sessionStorage.getItem('service-point-data'));
+                }
+                catch(error) {
+                    return false;
+                }
+
+                if (quote.shippingMethod()['carrier_code'] === 'sendcloud' && !servicePointData) {
+                    this.errorValidationMessage($t('Please select a service point'));
+
+                    return false;
+                }
+                return origResult;
+            }
+        });
+    }
+});
