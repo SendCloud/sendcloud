9f841122edc82b77b69647d8bf387c884f6a4fc5
rick <r.vanoosten@creativeict.nl>
2020-04-02 16:23:48 +0200

    bugfix | Replaced the payment overwrite with a mixin. This solves compatibility problems

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,7 +3,6 @@ var config = {
         "*": {
             'Magento_Checkout/js/model/shipping-save-processor/default': 'SendCloud_SendCloud/js/model/shipping-save-processor/servicepoint',
             'Amasty_Checkout/js/model/shipping-save-processor/default': 'SendCloud_SendCloud/js/model/shipping-save-processor/amasty-servicepoint',
-            'Magento_Checkout/js/view/payment/default': 'SendCloud_SendCloud/js/view/payment/default'
         }
     },
     config: {
@@ -13,6 +12,9 @@ var config = {
             },
             'Magento_Checkout/js/view/shipping': {
                 'SendCloud_SendCloud/js/mixins/checkout/view/shipping-mixin': true
+            },
+            'Magento_Checkout/js/view/payment/default': {
+                'SendCloud_SendCloud/js/mixins/checkout/view/payment/default-mixin': true
             },
             'Mageplaza_Osc/js/model/shipping-save-processor/checkout': {
                 'SendCloud_SendCloud/js/model/shipping-save-processor/mageplaza-servicepoint': true
