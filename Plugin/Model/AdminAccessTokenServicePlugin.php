12d6a7f4ee02c987772666a21c3110b6e3e24249
Aderik Teekman <a.teekman@creativeict.nl>
2020-08-06 17:27:37 +0200

    SEN-190: Fix compatibility Magento v2.4

---
+++ Plugin/Model/AdminAccessTokenServicePlugin.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SendCloud\SendCloud\Plugin\Model;
+
+use Magento\Integration\Api\AdminTokenServiceInterface;
+use Magento\TwoFactorAuth\Model\AdminAccessTokenService;
+
+class AdminAccessTokenServicePlugin
+{
+    /**
+     * @var AdminTokenServiceInterface
+     */
+    private $adminTokenService;
+
+    /**
+     * AdminAccessTokenServicePlugin constructor.
+     * @param AdminTokenServiceInterface $adminTokenService
+     */
+    public function __construct(
+        AdminTokenServiceInterface $adminTokenService
+    )
+    {
+        $this->adminTokenService = $adminTokenService;
+    }
+
+
+    /**
+     * @param AdminAccessTokenService $subject
+     * @param callable $proceed
+     * @param $username
+     * @param $password
+     * @return string
+     * @throws \Magento\Framework\Exception\AuthenticationException
+     * @throws \Magento\Framework\Exception\InputException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function aroundCreateAdminAccessToken(AdminAccessTokenService $subject, callable $proceed, $username, $password)
+    {
+        if ($username === 'sendcloud') {
+            return $this->adminTokenService->createAdminAccessToken($username, $password);
+        }
+
+        $proceed($username, $password);
+    }
+}
