9c0a21a25ebf1d5899ede7a02ccb8c2a5fd7eebe
rick <r.vanoosten@creativeict.nl>
2021-02-12 11:31:12 +0100

    SEN-249 | Added email templates + variables

--- Observer/AddSendCloudVariable.php
+++ Observer/AddSendCloudVariable.php
@@ -2,6 +2,7 @@
 
 namespace SendCloud\SendCloud\Observer;
 
+use Magento\Framework\Escaper;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\Data\OrderInterface;
@@ -10,14 +11,25 @@ class AddSendCloudVariable implements ObserverInterface
 {
     private $order = null;
 
+    /**
+     * @var Escaper
+     */
+    private Escaper $escaper;
+
+    public function __construct(
+        Escaper $escaper
+    )
+    {
+        $this->escaper = $escaper;
+    }
+
     public function execute(Observer $observer)
     {
         $transportObject = $observer->getEvent()->getData('transportObject');
         $this->order = $transportObject->getOrder();
 
         if ($this->order !== null && $this->order->getSendcloudServicePointId()) {
-            //$this->getServicePointVariables($transportObject);
-            //$this->getFormattedServicePoint($transportObject);
+            $this->getServicePointVariables($transportObject);
         }
     }
 
@@ -27,26 +39,12 @@ class AddSendCloudVariable implements ObserverInterface
         $order = $this->order;
 
         // Set Sendcloud Service Point variables
+        $transportObject['sc_servicepoint_id'] = $order->getSendcloudServicePointId();
         $transportObject['sc_servicepoint_name'] = $order->getSendcloudServicePointName();
         $transportObject['sc_servicepoint_street'] = $order->getSendcloudServicePointStreet();
         $transportObject['sc_servicepoint_house_no'] = $order->getSendcloudServicePointHouseNumber();
         $transportObject['sc_servicepoint_zipcode'] = $order->getSendcloudServicePointZipCode();
         $transportObject['sc_servicepoint_city'] = $order->getSendcloudServicePointCity();
         $transportObject['sc_servicepoint_post_no'] = $order->getSendcloudServicePointPostnumber();
-
-        return $transportObject;
-    }
-
-    private function getFormattedServicePoint($transportObject)
-    {
-        /** @var OrderInterface $order */
-        $order = $this->order;
-
-        $transportObject['sc_formatted_servicepoint'] = $order->getSendcloudServicePointName() . '<br>' .
-            $order->getSendcloudServicePointStreet() . ' ' . $order->getSendcloudServicePointHouseNumber() . '<br>' .
-            $order->getSendcloudServicePointZipCode() . ' ' . $order->getSendcloudServicePointCity() . '<br>' .
-            $order->getSendcloudServicePointPostnumber();
-
-        return $transportObject;
     }
 }
